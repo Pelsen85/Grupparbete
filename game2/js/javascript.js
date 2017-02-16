a954626f6c14b4d9d594ef34c78f8be8e71d630b
Adrian Newton-Ohnell <Adrian Newton-Ohnell>
2017-02-16 08:46:05 +0100

    Tic Tac Baby

--- game2/js/javascript.js
+++ game2/js/javascript.js
@@ -1,4 +1,4 @@
-//Global Variables
+//Global Variables 
         var painted;
 
         var content;
@@ -30,7 +30,8 @@
             painted = new Array();
 
             content = new Array();
-
+            
+            // winningCombinations is a multi-dimensional array
             winningCombinations = [[0,1,2],[3,4,5],[6,7,8],[0,3,6],[1,4,7],[2,5,8],[0,4,8],[2,4,6]];
 
             for(var l = 0; l <= 8; l++){
@@ -47,34 +48,46 @@
 
 //Game methods
 
+        
+        // canvasClicked gives the number that is sent from the body of the html to the variable canvasNumber
         function canvasClicked(canvasNumber){
 
+            //This is where we can see which canvas that is being used
             theCanvas = "canvas"+canvasNumber;
-
+            // c will now hold the canvas that is being used
             c = document.getElementById(theCanvas);
-
+            // Here the context of the canvas is given
             cxt = c.getContext("2d");
 
  
-
+            // Here it is checked if the canvas has already been drawn on
             if(painted[canvasNumber-1] ==false){
-
+                
+                // Here it is decided whos turn it is
                 if(turn%2==0){
-
+                    
+                    // This makes sure that the symbol is drawn
                     cxt.beginPath();
-
+                    
+                    // This is a specific location in the canvas
                     cxt.moveTo(10,10);
-
+                    
+                    // This draws a line from 10,10 to 40,40
                     cxt.lineTo(40,40);
-
+                    
+                    // This is another specific location in the canvac
                     cxt.moveTo(40,10);
-
+                    
+                    // This draws another line
                     cxt.lineTo(10,40);
 
+                    //  This will actually draw the line
                     cxt.stroke();
-
+                    
+                    // This makes sure that the imaginary cursoer is removed
                     cxt.closePath();
-
+                    
+                    // This makes sure an X is drawn
                     content[canvasNumber-1] = 'X';
 
                 }
@@ -82,7 +95,8 @@
  
 
                 else{
-
+                    
+                    // If it isn't X's turn, it is O's turn. So a circle is drawn instead.
                     cxt.beginPath();
 
                     cxt.arc(25,25,20,0,Math.PI*2,true);
@@ -96,13 +110,14 @@
                 }
 
  
-
+                // The turn is incremented so that the next turn will be the other player
                 turn++;
 
                 painted[canvasNumber-1] = true;
 
                 squaresFilled++;
-
+                
+                // This function gets the content of the current canvas so it knows which player who just had a turn
                 checkForWinners(content[canvasNumber-1]);
 
  
@@ -120,7 +135,8 @@
             }
 
             else{
-
+                
+                // This check that the canvas that is chosen is not already drawn
                 alert("THAT SPACE IS ALREADY OCCUPIED WITH YOUR HEART!");
 
             }
@@ -130,7 +146,7 @@
         }
 
  
-
+// This function checks the last dran symbol so that it knows what player who just made a move.
 function checkForWinners(symbol){
 
              
@@ -150,7 +166,7 @@ function checkForWinners(symbol){
  }
 
  
-
+// This enables players to play again
 function playAgain(){
 
             y=confirm("PLAY AGAIN?");
